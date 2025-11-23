a223bec9f0850261b211b84eda0d42ff6acbcd03
Badresh Kumar Singh <badreshkumarsingh@gmail.com>
2025-11-23 19:58:08 +0530

    Learning Winston Library

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,22 +1,34 @@
 import fs from "fs";
+import winston from "winston";
 
 const fsPromise = fs.promises;
 
-async function log(logData) {
-    try {
-        logData = `\n${new Date().toString()}. Log Data: ${logData}`;
-        // Append the log data to the file
-        await fsPromise.appendFile('log.txt', logData);
-    } catch(err) {
-        console.log(err);
-    }
-}
+// async function log(logData) {
+//     try {
+//         logData = `\n${new Date().toString()}. Log Data: ${logData}`;
+//         // Append the log data to the file
+//         await fsPromise.appendFile('log.txt', logData);
+//     } catch(err) {
+//         console.log(err);
+//     }
+// }
 
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'request-logging' },
+    transports: [
+        new winston.transports.File({ filename: 'logs.txt' })
+    ]
+});
+
+// const loggerMiddleware = async (req, res, next) => {
 const loggerMiddleware = async (req, res, next) => {
     if(!req.url.includes('signin')) {
         const logData = `${req.url} - ${JSON.stringify(req.body)}`;
-        console.log(logData);
-        await log(logData);
+        // console.log(logData);
+        // await log(logData);
+        logger.info(logData);
     }
     next();
 }
